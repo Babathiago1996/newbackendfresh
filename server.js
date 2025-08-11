8ec35ade8c32fe7d789ff7d8379ebde439a31168
unknown <Adebisiazeez1997@gmail.com>
2025-08-11 11:10:30 +0100

    updated

--- server.js
+++ server.js
@@ -16,10 +16,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "https://newfrontend-brown.vercel.app/login",
-      "http://localhost:5173",
-    ],
+    origin: ["https://newfrontend-brown.vercel.app", "http://localhost:5173"],
     credentials: true,
   })
 );
